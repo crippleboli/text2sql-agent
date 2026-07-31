24959764ce72760153ae7ac359eb5328032e24ee
crippleboli <113300829+crippleboli@users.noreply.github.com>
2026-07-31 15:06:02 +0800

    feat: 日志系统

---
+++ app/core/log.py
@@ -0,0 +1,72 @@
+import asyncio
+import sys
+import uuid
+from pathlib import Path
+from loguru import logger
+from app.conf.app_config import app_config
+from app.core.context import request_id_ctx_var
+
+
+# 日志模板
+log_format = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+    "<level>{level: <8}</level> | "
+    "<magenta>request_id - {extra[request_id]}</magenta> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+    "<level>{message}</level>"
+)
+
+
+def inject_request_id(record):
+    try:
+        request_id = request_id_ctx_var.get()
+    except Exception as e:
+        request_id = uuid.uuid4()
+    record["extra"]["request_id"] = request_id
+
+
+# 移除初始配置
+logger.remove()
+# 添加自写函数
+logger = logger.patch(inject_request_id)
+if app_config.logging.console.enable:
+    logger.add(sink=sys.stdout, level=app_config.logging.console.level, format=log_format)
+
+if app_config.logging.file.enable:
+    path = Path(app_config.logging.file.path)
+    path.mkdir(parents=True, exist_ok=True)     # 确保路径存在
+    logger.add(
+        sink=path / "app.log",
+        level=app_config.logging.file.level,
+        format=log_format,
+        rotation=app_config.logging.file.rotation,
+        retention=app_config.logging.file.retention,
+        encoding="utf-8"
+    )
+
+if __name__ == '__main__':
+    async def test1():
+        # 接收请求
+        request_id_ctx_var.set('request-1')
+
+        # 模拟处理
+        await asyncio.sleep(1)
+        await graph('request-1')
+
+
+    async def test2():
+        # 接收请求
+        request_id_ctx_var.set('request-2')
+
+        # 模拟处理
+        await asyncio.sleep(1)
+        await graph('request-2')
+
+    async def graph(request:str):
+        # 打印日志
+        logger.info(request)
+
+    async def main():
+        await asyncio.gather(test1(), test2())
+
+    asyncio.run(main())
